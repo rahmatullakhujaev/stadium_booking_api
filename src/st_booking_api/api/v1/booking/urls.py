d15c044818de559e113328ed9744ef2790345e4a
rahmatullakhujaev <r.xo'jayev@student.inha.uz>
2024-05-15 15:15:43 +0500

    done

--- src/st_booking_api/api/v1/booking/urls.py
+++ src/st_booking_api/api/v1/booking/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import BookingViewSet, AdminViewSet
 
 router = DefaultRouter()
-router.register(r'my_booking', BookingViewSet)
-router.register(r'admin', AdminViewSet)
+router.register(r'my_booking', BookingViewSet, basename='booking')
+router.register(r'admin', AdminViewSet, basename='admin-booking')
 urlpatterns = [
     path('', include(router.urls))
 
