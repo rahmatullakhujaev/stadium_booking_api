412c60f3922c82302f0ad323393ab0b0aab1af0a
rahmatullakhujaev <r.xo'jayev@student.inha.uz>
2024-07-16 12:41:47 +0500

    swagger added

--- src/st_booking_api/urls.py
+++ src/st_booking_api/urls.py
@@ -21,10 +21,25 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Stadium Booking API",
+        default_version='v1',
+        description="Test description",
+        contact=openapi.Contact(email="rahmatullokhujaev@icloud.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include(api_urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
